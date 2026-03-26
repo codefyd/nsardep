0bc19099e21f7504b2b5df76b3d0ec2da4635210
codefyd <ahsnsy77@gmail.com>
2026-03-26 05:40:28 +0300

    Create config.js

---
+++ config.js
@@ -0,0 +1,6 @@
+// config.js
+// ضع رابط Google Apps Script Web App الخاص بك هنا
+const API_URL = 'https://script.google.com/macros/s/AKfycbxWLcuF8aDcfWEQB3dAtcNdP50Q314S8OsknzULfTZlov9_dZAdvfw-u0XgOTALiPHY/exec';
+
+// لا تقم بتعديل ما دون هذا السطر
+window.APP_CONFIG = { API_URL };
